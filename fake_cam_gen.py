f9d1bf7b1c3726cb6354980a8622fba288721e23
Jan <j.schiff96@gmail.com>
2021-03-08 13:43:29 +0100

    added fake_cam generator

---
+++ fake_cam_gen.py
@@ -0,0 +1,27 @@
+"""
+Created by Jan Schiffeler on 08.03.21
+jan.schiffeler[at]gmail.com
+
+Changed by
+
+
+
+Python 3.
+Library version:
+
+
+"""
+
+import torch
+import numpy as np
+import cv2
+import glob
+import os.path as osp
+
+for img_path in glob.glob("fake_cam/*.png"):
+    img = cv2.imread(img_path, 0)
+    keys = torch.tensor([0])
+    cam = cv2.resize(img, (125, 94))
+    cam = torch.from_numpy(cam[np.newaxis, :, :])
+    cam_npy = {'keys': keys, 'cam': cam, 'high_res': img}
+    np.save(f"results/cam/{osp.basename(img_path)[:-4]}.npy", cam_npy)
