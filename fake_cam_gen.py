3008ade47c0f032781f490a1f72ef81da95e8cae
Jan <j.schiff96@gmail.com>
2021-03-15 13:45:49 +0100

    added edge detection

--- fake_cam_gen.py
+++ fake_cam_gen.py
@@ -29,11 +29,18 @@ for img_path in glob.glob("boulderDataSet/images/*.png"):
 
     equalized_colour = cv2.merge(cl_img)
 
+    edges_img = cv2.pyrDown(cv2.pyrDown(cv2.cvtColor(equalized_colour, cv2.COLOR_BGR2GRAY)))
+    edges = cv2.Canny(edges_img, 100, 200)
+    edges = cv2.GaussianBlur(edges, (11, 11), 0)
+    edges = cv2.resize(edges, (500, 375)).astype("float32") / 255
+
     saliency = cv2.saliency.StaticSaliencySpectralResidual_create()
     (success, saliencyMap) = saliency.computeSaliency(equalized_colour)
-    cam = cv2.resize(saliencyMap, (125, 94))
+
+    activation_map = cv2.addWeighted(saliencyMap, 0.8, edges, 0.2, 0.0)
+    cam = cv2.resize(activation_map, (125, 94))
     cam = torch.from_numpy(cam[np.newaxis, :, :])
 
-    cam_npy = {'keys': torch.tensor([0]), 'cam': cam, 'high_res': cv2.resize(saliencyMap, (500, 375))[np.newaxis, :, :]}
+    cam_npy = {'keys': torch.tensor([0]), 'cam': cam, 'high_res': cv2.resize(activation_map, (500, 375))[np.newaxis, :, :]}
     np.save(f"result/cam/{osp.basename(img_path)[:-4]}.npy", cam_npy)
     print(img_path)
